270e6865a6841af7d9d1fad4229261b6cf1dcbea
AgyCoder <1292286139@qq.com>
2019-06-18 21:17:48 +0800

    添加了查找学生和老师详细信息----洪俐

--- src/main/java/com/competition/competitionsys/controller/UserController.java
+++ src/main/java/com/competition/competitionsys/controller/UserController.java
@@ -5,6 +5,7 @@ import com.competition.competitionsys.domain.VO.ResponseData;
 import com.competition.competitionsys.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,4 +23,13 @@ public class UserController {
         return userService.findAllUsers();
     }
 
+    @RequestMapping("/findStudent/{userCode}")
+    public ResponseData findStudent(@PathVariable("userCode") Integer userCode){
+        return userService.findStudentByUserCode(userCode);
+    }
+
+    @RequestMapping("/findTeacher/{userCode}")
+    public ResponseData findTeacher(@PathVariable("userCode") Integer userCode){
+        return userService.findStudentByUserCode(userCode);
+    }
 }
